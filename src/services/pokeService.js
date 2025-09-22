05a4a90b84b59474d96168d2f155a8451ed57f4e
Charls Cabangon <charlsmercabangon@gmail.com>
2025-09-22 21:21:22 +0800

    some utilities and api

---
+++ src/services/pokeService.js
@@ -0,0 +1,24 @@
+
+async function fetchPokemon(id) {
+  const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const data = await promise.json();
+
+  return {
+    id: data.id,
+    name: data.name,
+    image: data.sprites.other['official-artwork'].front_default,
+  }
+}
+
+export default async function fetchPokemons(count) {
+  const promises = [];
+
+  for (let i = 1; i <= count; i++) {
+    const randomId = Math.floor(Math.random() * 898) + 1;
+    promises.push(fetchPokemon(randomId));
+  }
+
+  const pokemons = await Promise.all(promises);
+
+  return pokemons;
+}
---
+++ src/utils/levels.js
@@ -0,0 +1,5 @@
+export const LEVELS = {
+  EASY: { level: "Easy", count: 5 },
+  MEDIUM: { level: "Medium", count: 10 },
+  HARD: { level: "Hard", count: 20 },
+};
